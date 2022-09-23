c7fae74ef60c09c4b3854a112ecd953af7371ec7
初十 <392706283@qq.com>
2022-09-24 00:38:38 +0800

    ui-调整

--- src/Daily-Newspaper-Tools/MainForm.Designer.cs
+++ src/Daily-Newspaper-Tools/MainForm.Designer.cs
@@ -29,20 +29,39 @@ namespace Daily_Newspaper_Tools
         /// </summary>
         private void InitializeComponent()
         {
+            this.uiAvatar1 = new Sunny.UI.UIAvatar();
+            this.Header.SuspendLayout();
             this.SuspendLayout();
             // 
             // Header
             // 
+            this.Header.BackColor = System.Drawing.Color.White;
+            this.Header.Controls.Add(this.uiAvatar1);
             this.Header.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
             this.Header.ForeColor = System.Drawing.Color.White;
-            this.Header.Size = new System.Drawing.Size(774, 57);
+            this.Header.Location = new System.Drawing.Point(320, 35);
+            this.Header.Size = new System.Drawing.Size(704, 72);
             this.Header.Style = Sunny.UI.UIStyle.Custom;
             this.Header.StyleCustomMode = true;
             // 
             // Aside
             // 
-            this.Aside.Size = new System.Drawing.Size(250, 685);
+            this.Aside.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(182)))));
+            this.Aside.MenuStyle = Sunny.UI.UIMenuStyle.Custom;
+            this.Aside.Size = new System.Drawing.Size(320, 685);
             this.Aside.MenuItemClick += new Sunny.UI.UINavMenu.OnMenuItemClick(this.Aside_MenuItemClick);
+            // 
+            // uiAvatar1
+            // 
+            this.uiAvatar1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiAvatar1.Location = new System.Drawing.Point(699, 6);
+            this.uiAvatar1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar1.Name = "uiAvatar1";
+            this.uiAvatar1.Size = new System.Drawing.Size(60, 60);
+            this.uiAvatar1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiAvatar1.TabIndex = 0;
+            this.uiAvatar1.Text = "uiAvatar1";
+            this.uiAvatar1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
             // 
             // MainForm
             // 
@@ -52,10 +71,13 @@ namespace Daily_Newspaper_Tools
             this.Text = "FAsideHeaderMain";
             this.ZoomScaleRect = new System.Drawing.Rectangle(-98, 15, 1024, 720);
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Header.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Sunny.UI.UIAvatar uiAvatar1;
     }
 }
