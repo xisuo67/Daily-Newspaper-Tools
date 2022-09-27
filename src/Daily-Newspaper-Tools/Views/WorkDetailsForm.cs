096dd716eef20977ed5c337d461e3ea7b0d762f1
初十 <392706283@qq.com>
2022-09-27 17:40:21 +0800

    升级4.8，增加helper方法

--- src/Daily-Newspaper-Tools/Views/WorkDetailsForm.cs
+++ src/Daily-Newspaper-Tools/Views/WorkDetailsForm.cs
@@ -260,7 +260,67 @@ namespace Daily_Newspaper_Tools.Views
         /// <param name="e"></param>
         private void uiSymbolBtnSendEmail_Click(object sender, EventArgs e)
         {
+            //button4.Enabled = false;
+            //var weekWorks = this.richTextBox1.Rtf;
+            //string contacts = string.Empty;
+            //if (string.IsNullOrEmpty(this.richTextBox1.Text))
+            //{
+            //    MessageBox.Show("请先生成周报再发送");
+            //}
+            //else
+            //{
+            //    string content = RtfToHtmlConverter.ConvertRtfToHtml(weekWorks);
+            //    using (var ctx = new EntityContext())
+            //    {
+            //        var emailConfig = ctx.EmailConfigs.FirstOrDefault();
+            //        if (emailConfig != null)
+            //        {
+            //            contacts = string.Join(";", ctx.Contacts.Select(x => x.Email));
+            //            if (string.IsNullOrEmpty(contacts))
+            //            {
+            //                MessageBox.Show("请先配置联系人邮箱");
+            //            }
+            //            var emailTaskConfig = ctx.EmailTaskConfigs.FirstOrDefault();
+            //            if (emailTaskConfig != null)
+            //            {
+            //                //立即发送
+            //                if (emailTaskConfig.IsSendNow == true)
+            //                {
+            //                    SendEmail sendEmail = new SendEmail(emailConfig.EmailAddress, emailConfig.Email_Server, emailConfig.Email_LoginId, emailConfig.Email_LoginPwd);
+            //                    bool isSend = sendEmail.Send(contacts, "周报", content);
+            //                    if (isSend)
+            //                        MessageBox.Show("发送成功");
+            //                    else
 
+            //                        MessageBox.Show("发送失败");
+            //                }
+            //                else
+            //                {
+            //                    var taskTime = (DateTime)emailTaskConfig.TaskTime;
+            //                    DateTime sendTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, taskTime.Hour, taskTime.Minute, taskTime.Second);
+            //                    EmailTask emailTask = new EmailTask()
+            //                    {
+            //                        EmailContent = content,
+            //                        IsSend = false,
+            //                        SendTime = sendTime
+            //                    };
+            //                    ctx.EmailTasks.Add(emailTask);
+            //                    MessageBox.Show($"任务设置成功，邮件将于今晚发送");
+            //                }
+            //            }
+            //            else
+            //            {
+            //                MessageBox.Show("请先配置邮件设置");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("请先配置联系人邮箱");
+            //        }
+            //        ctx.SaveChanges();
+            //    }
+            //}
+            //button4.Enabled = true;
         }
         #endregion
 
---
+++ src/ToolsHelper/RtfToHtmlConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WorkTools
+{
+    public static class RtfToHtmlConverter
+    {
+        private const string FlowDocumentFormat = "<FlowDocument>{0}</FlowDocument>";
+
+        public static string ConvertRtfToHtml(string rtfText)
+        {
+            var xamlText = string.Format(FlowDocumentFormat, ConvertRtfToXaml(rtfText));
+
+            return HtmlFromXamlConverter.ConvertXamlToHtml(xamlText, false);
+        }
+
+        private static string ConvertRtfToXaml(string rtfText)
+        {
+            var richTextBox = new RichTextBox();
+            if (string.IsNullOrEmpty(rtfText)) return "";
+
+            var textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+
+            //Create a MemoryStream of the Rtf content
+
+            using (var rtfMemoryStream = new MemoryStream())
+            {
+                using (var rtfStreamWriter = new StreamWriter(rtfMemoryStream))
+                {
+                    rtfStreamWriter.Write(rtfText);
+                    rtfStreamWriter.Flush();
+                    rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+
+                    //Load the MemoryStream into TextRange ranging from start to end of RichTextBox.
+                    textRange.Load(rtfMemoryStream, "Rich Text Format");
+                }
+            }
+
+            using (var rtfMemoryStream = new MemoryStream())
+            {
+
+                textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                textRange.Save(rtfMemoryStream, "Xaml");
+                rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+                using (var rtfStreamReader = new StreamReader(rtfMemoryStream))
+                {
+                    return rtfStreamReader.ReadToEnd();
+                }
+            }
+
+        }
+
+
+
+
+    }
+}
