43094c9f0e4eb3b9ebb01a7927acd1c2929b9e41
初十 <392706283@qq.com>
2022-09-28 21:07:19 +0800

    新增组织架构

---
+++ src/Daily-Newspaper-Tools/Views/OrganizationUnitForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Daily_Newspaper_Tools.Views
+{
+    public partial class OrganizationUnitForm : Form
+    {
+        public OrganizationUnitForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
