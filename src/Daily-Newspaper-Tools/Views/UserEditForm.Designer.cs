0791fbc57ddda8c7a6589abf526ce72d8d7e58c3
初十 <392706283@qq.com>
2022-09-29 11:15:41 +0800

    新增用户编辑页面

---
+++ src/Daily-Newspaper-Tools/Views/UserEditForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Daily_Newspaper_Tools.Views
+{
+    partial class UserEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.uiLabel2 = new Sunny.UI.UILabel();
+            this.uiTxtUserName = new Sunny.UI.UITextBox();
+            this.uiTxtPassword = new Sunny.UI.UITextBox();
+            this.uiCmbTreeDepartment = new Sunny.UI.UIComboTreeView();
+            this.uiTxtName = new Sunny.UI.UITextBox();
+            this.uiLabel4 = new Sunny.UI.UILabel();
+            this.uiLabel3 = new Sunny.UI.UILabel();
+            this.pnlBtm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBtm
+            // 
+            this.pnlBtm.Location = new System.Drawing.Point(1, 311);
+            this.pnlBtm.Size = new System.Drawing.Size(572, 55);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(444, 12);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(329, 12);
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.AutoSize = true;
+            this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel1.Location = new System.Drawing.Point(21, 210);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(90, 21);
+            this.uiLabel1.TabIndex = 2;
+            this.uiLabel1.Text = "登录账号：";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLabel2
+            // 
+            this.uiLabel2.AutoSize = true;
+            this.uiLabel2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel2.Location = new System.Drawing.Point(21, 270);
+            this.uiLabel2.Name = "uiLabel2";
+            this.uiLabel2.Size = new System.Drawing.Size(90, 21);
+            this.uiLabel2.TabIndex = 3;
+            this.uiLabel2.Text = "登录密码：";
+            this.uiLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTxtUserName
+            // 
+            this.uiTxtUserName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTxtUserName.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTxtUserName.Location = new System.Drawing.Point(135, 202);
+            this.uiTxtUserName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTxtUserName.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uiTxtUserName.Name = "uiTxtUserName";
+            this.uiTxtUserName.ShowText = false;
+            this.uiTxtUserName.Size = new System.Drawing.Size(416, 29);
+            this.uiTxtUserName.TabIndex = 6;
+            this.uiTxtUserName.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTxtUserName.Visible = false;
+            this.uiTxtUserName.Watermark = "";
+            this.uiTxtUserName.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTxtPassword
+            // 
+            this.uiTxtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTxtPassword.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTxtPassword.Location = new System.Drawing.Point(135, 270);
+            this.uiTxtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTxtPassword.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uiTxtPassword.Name = "uiTxtPassword";
+            this.uiTxtPassword.ShowText = false;
+            this.uiTxtPassword.Size = new System.Drawing.Size(416, 29);
+            this.uiTxtPassword.TabIndex = 7;
+            this.uiTxtPassword.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTxtPassword.Watermark = "请输入密码";
+            this.uiTxtPassword.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiCmbTreeDepartment
+            // 
+            this.uiCmbTreeDepartment.DropDownStyle = Sunny.UI.UIDropDownStyle.DropDownList;
+            this.uiCmbTreeDepartment.FillColor = System.Drawing.Color.White;
+            this.uiCmbTreeDepartment.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCmbTreeDepartment.Location = new System.Drawing.Point(135, 134);
+            this.uiCmbTreeDepartment.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiCmbTreeDepartment.MinimumSize = new System.Drawing.Size(63, 0);
+            this.uiCmbTreeDepartment.Name = "uiCmbTreeDepartment";
+            this.uiCmbTreeDepartment.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
+            this.uiCmbTreeDepartment.Size = new System.Drawing.Size(416, 27);
+            this.uiCmbTreeDepartment.TabIndex = 12;
+            this.uiCmbTreeDepartment.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiCmbTreeDepartment.Watermark = "";
+            this.uiCmbTreeDepartment.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTxtName
+            // 
+            this.uiTxtName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTxtName.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTxtName.Location = new System.Drawing.Point(135, 65);
+            this.uiTxtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTxtName.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uiTxtName.Name = "uiTxtName";
+            this.uiTxtName.ShowText = false;
+            this.uiTxtName.Size = new System.Drawing.Size(416, 29);
+            this.uiTxtName.TabIndex = 11;
+            this.uiTxtName.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTxtName.Watermark = "";
+            this.uiTxtName.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLabel4
+            // 
+            this.uiLabel4.AutoSize = true;
+            this.uiLabel4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel4.Location = new System.Drawing.Point(21, 134);
+            this.uiLabel4.Name = "uiLabel4";
+            this.uiLabel4.Size = new System.Drawing.Size(90, 21);
+            this.uiLabel4.TabIndex = 10;
+            this.uiLabel4.Text = "所属部门：";
+            this.uiLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel4.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLabel3
+            // 
+            this.uiLabel3.AutoSize = true;
+            this.uiLabel3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel3.Location = new System.Drawing.Point(21, 65);
+            this.uiLabel3.Name = "uiLabel3";
+            this.uiLabel3.Size = new System.Drawing.Size(74, 21);
+            this.uiLabel3.TabIndex = 9;
+            this.uiLabel3.Text = "用户名：";
+            this.uiLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // UserEditForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(574, 369);
+            this.Controls.Add(this.uiCmbTreeDepartment);
+            this.Controls.Add(this.uiTxtName);
+            this.Controls.Add(this.uiLabel4);
+            this.Controls.Add(this.uiLabel3);
+            this.Controls.Add(this.uiTxtPassword);
+            this.Controls.Add(this.uiTxtUserName);
+            this.Controls.Add(this.uiLabel2);
+            this.Controls.Add(this.uiLabel1);
+            this.Name = "UserEditForm";
+            this.Text = "编辑用户信息";
+            this.ZoomScaleRect = new System.Drawing.Rectangle(15, 15, 800, 450);
+            this.Controls.SetChildIndex(this.uiLabel1, 0);
+            this.Controls.SetChildIndex(this.uiLabel2, 0);
+            this.Controls.SetChildIndex(this.uiTxtUserName, 0);
+            this.Controls.SetChildIndex(this.uiTxtPassword, 0);
+            this.Controls.SetChildIndex(this.pnlBtm, 0);
+            this.Controls.SetChildIndex(this.uiLabel3, 0);
+            this.Controls.SetChildIndex(this.uiLabel4, 0);
+            this.Controls.SetChildIndex(this.uiTxtName, 0);
+            this.Controls.SetChildIndex(this.uiCmbTreeDepartment, 0);
+            this.pnlBtm.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UILabel uiLabel1;
+        private Sunny.UI.UILabel uiLabel2;
+        private Sunny.UI.UITextBox uiTxtUserName;
+        private Sunny.UI.UITextBox uiTxtPassword;
+        private Sunny.UI.UIComboTreeView uiCmbTreeDepartment;
+        private Sunny.UI.UITextBox uiTxtName;
+        private Sunny.UI.UILabel uiLabel4;
+        private Sunny.UI.UILabel uiLabel3;
+    }
+}
---
+++ src/Daily-Newspaper-Tools/Views/UserEditForm.cs
@@ -0,0 +1,50 @@
+﻿using Daily_Newspaper_Tools.Common.Login;
+using DAL.Entity;
+using Sunny.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Daily_Newspaper_Tools.Views
+{
+    public partial class UserEditForm : UIEditForm
+    {
+        public UserEditForm()
+        {
+            InitializeComponent();
+        }
+        protected override bool CheckData()
+        {
+            return CheckEmpty(uiTxtName, "请输入用户名")
+                   && CheckEmpty(uiTxtPassword, "请输入密码");
+        }
+        private User user;
+        public User User
+        {
+            get
+            {
+                if (user == null)
+                {
+                    user = new User();
+                }
+                user.Password = uiTxtPassword.Text.Trim();
+                user.Name = uiTxtName.Text.Trim();
+                //部门后面迭代完成
+                return user;
+            }
+            set
+            {
+                user = value;
+                uiTxtPassword.Text = value.Password;
+                uiTxtName.Text = value.Name;
+                uiTxtUserName.Text = value.UserName;
+            }
+        }
+    }
+}
