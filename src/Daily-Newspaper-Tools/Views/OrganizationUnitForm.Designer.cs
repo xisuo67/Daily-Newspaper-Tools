6fb822892d6d75b7bb066ae1375840d256037a8c
初十 <392706283@qq.com>
2022-09-30 20:58:09 +0800

    ui-调整

--- src/Daily-Newspaper-Tools/Views/OrganizationUnitForm.Designer.cs
+++ src/Daily-Newspaper-Tools/Views/OrganizationUnitForm.Designer.cs
@@ -28,98 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.uiPanelLeft = new Sunny.UI.UIPanel();
+            this.uiPanelLeftHead = new Sunny.UI.UIPanel();
             this.uiPanel1 = new Sunny.UI.UIPanel();
-            this.uiPanel2 = new Sunny.UI.UIPanel();
-            this.uiPanel3 = new Sunny.UI.UIPanel();
-            this.uiLabel1 = new Sunny.UI.UILabel();
-            this.uiButton1 = new Sunny.UI.UIButton();
-            this.uiButton2 = new Sunny.UI.UIButton();
             this.uiTreeView1 = new Sunny.UI.UITreeView();
-            this.uiPanel2.SuspendLayout();
-            this.uiPanel3.SuspendLayout();
+            this.uiButton2 = new Sunny.UI.UIButton();
+            this.uiButton1 = new Sunny.UI.UIButton();
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.uiTabControl1 = new Sunny.UI.UITabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.uiPanelLeft.SuspendLayout();
+            this.uiPanelLeftHead.SuspendLayout();
+            this.uiPanel1.SuspendLayout();
+            this.uiTabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // uiPanelLeft
+            // 
+            this.uiPanelLeft.Controls.Add(this.uiTreeView1);
+            this.uiPanelLeft.Controls.Add(this.uiPanelLeftHead);
+            this.uiPanelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.uiPanelLeft.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiPanelLeft.Location = new System.Drawing.Point(0, 35);
+            this.uiPanelLeft.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiPanelLeft.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiPanelLeft.Name = "uiPanelLeft";
+            this.uiPanelLeft.Size = new System.Drawing.Size(249, 415);
+            this.uiPanelLeft.TabIndex = 1;
+            this.uiPanelLeft.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uiPanelLeft.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiPanelLeftHead
+            // 
+            this.uiPanelLeftHead.Controls.Add(this.uiButton2);
+            this.uiPanelLeftHead.Controls.Add(this.uiButton1);
+            this.uiPanelLeftHead.Controls.Add(this.uiLabel1);
+            this.uiPanelLeftHead.Dock = System.Windows.Forms.DockStyle.Top;
+            this.uiPanelLeftHead.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiPanelLeftHead.Location = new System.Drawing.Point(0, 0);
+            this.uiPanelLeftHead.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiPanelLeftHead.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiPanelLeftHead.Name = "uiPanelLeftHead";
+            this.uiPanelLeftHead.Size = new System.Drawing.Size(249, 47);
+            this.uiPanelLeftHead.TabIndex = 0;
+            this.uiPanelLeftHead.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uiPanelLeftHead.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
             // 
             // uiPanel1
             // 
-            this.uiPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiPanel1.Controls.Add(this.uiTabControl1);
+            this.uiPanel1.Dock = System.Windows.Forms.DockStyle.Right;
             this.uiPanel1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiPanel1.Location = new System.Drawing.Point(0, 0);
+            this.uiPanel1.Location = new System.Drawing.Point(252, 35);
             this.uiPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.uiPanel1.MinimumSize = new System.Drawing.Size(1, 1);
             this.uiPanel1.Name = "uiPanel1";
-            this.uiPanel1.Size = new System.Drawing.Size(800, 450);
+            this.uiPanel1.Size = new System.Drawing.Size(548, 415);
             this.uiPanel1.TabIndex = 0;
             this.uiPanel1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
             this.uiPanel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
-            // 
-            // uiPanel2
-            // 
-            this.uiPanel2.Controls.Add(this.uiTreeView1);
-            this.uiPanel2.Controls.Add(this.uiPanel3);
-            this.uiPanel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.uiPanel2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiPanel2.Location = new System.Drawing.Point(0, 0);
-            this.uiPanel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.uiPanel2.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiPanel2.Name = "uiPanel2";
-            this.uiPanel2.Size = new System.Drawing.Size(256, 450);
-            this.uiPanel2.TabIndex = 1;
-            this.uiPanel2.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
-            this.uiPanel2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
-            // 
-            // uiPanel3
-            // 
-            this.uiPanel3.Controls.Add(this.uiButton2);
-            this.uiPanel3.Controls.Add(this.uiButton1);
-            this.uiPanel3.Controls.Add(this.uiLabel1);
-            this.uiPanel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.uiPanel3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiPanel3.Location = new System.Drawing.Point(0, 0);
-            this.uiPanel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.uiPanel3.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiPanel3.Name = "uiPanel3";
-            this.uiPanel3.Size = new System.Drawing.Size(256, 47);
-            this.uiPanel3.TabIndex = 0;
-            this.uiPanel3.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
-            this.uiPanel3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
-            // 
-            // uiLabel1
-            // 
-            this.uiLabel1.AutoSize = true;
-            this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiLabel1.Location = new System.Drawing.Point(3, 9);
-            this.uiLabel1.Name = "uiLabel1";
-            this.uiLabel1.Size = new System.Drawing.Size(74, 21);
-            this.uiLabel1.TabIndex = 0;
-            this.uiLabel1.Text = "组织架构";
-            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.uiLabel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
-            // 
-            // uiButton1
-            // 
-            this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.uiButton1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiButton1.Location = new System.Drawing.Point(74, 9);
-            this.uiButton1.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiButton1.Name = "uiButton1";
-            this.uiButton1.Size = new System.Drawing.Size(77, 21);
-            this.uiButton1.TabIndex = 1;
-            this.uiButton1.Text = "新增节点";
-            this.uiButton1.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiButton1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
-            // 
-            // uiButton2
-            // 
-            this.uiButton2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.uiButton2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiButton2.Location = new System.Drawing.Point(157, 9);
-            this.uiButton2.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiButton2.Name = "uiButton2";
-            this.uiButton2.Size = new System.Drawing.Size(84, 21);
-            this.uiButton2.TabIndex = 2;
-            this.uiButton2.Text = "编辑节点";
-            this.uiButton2.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiButton2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
             // 
             // uiTreeView1
             // 
@@ -132,35 +100,117 @@
             this.uiTreeView1.MinimumSize = new System.Drawing.Size(1, 1);
             this.uiTreeView1.Name = "uiTreeView1";
             this.uiTreeView1.ShowText = false;
-            this.uiTreeView1.Size = new System.Drawing.Size(256, 403);
+            this.uiTreeView1.Size = new System.Drawing.Size(249, 368);
             this.uiTreeView1.TabIndex = 1;
             this.uiTreeView1.Text = "uiTreeView1";
             this.uiTreeView1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
             this.uiTreeView1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
             // 
+            // uiButton2
+            // 
+            this.uiButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton2.Location = new System.Drawing.Point(161, 14);
+            this.uiButton2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton2.Name = "uiButton2";
+            this.uiButton2.Size = new System.Drawing.Size(84, 21);
+            this.uiButton2.TabIndex = 5;
+            this.uiButton2.Text = "编辑节点";
+            this.uiButton2.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiButton1
+            // 
+            this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.Location = new System.Drawing.Point(78, 14);
+            this.uiButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton1.Name = "uiButton1";
+            this.uiButton1.Size = new System.Drawing.Size(77, 21);
+            this.uiButton1.TabIndex = 4;
+            this.uiButton1.Text = "新增节点";
+            this.uiButton1.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.AutoSize = true;
+            this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel1.Location = new System.Drawing.Point(7, 14);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(74, 21);
+            this.uiLabel1.TabIndex = 3;
+            this.uiLabel1.Text = "组织架构";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiTabControl1
+            // 
+            this.uiTabControl1.Controls.Add(this.tabPage1);
+            this.uiTabControl1.Controls.Add(this.tabPage2);
+            this.uiTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiTabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.uiTabControl1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTabControl1.Frame = null;
+            this.uiTabControl1.ItemSize = new System.Drawing.Size(150, 40);
+            this.uiTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.uiTabControl1.MainPage = "";
+            this.uiTabControl1.Name = "uiTabControl1";
+            this.uiTabControl1.SelectedIndex = 0;
+            this.uiTabControl1.Size = new System.Drawing.Size(548, 415);
+            this.uiTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.uiTabControl1.TabIndex = 0;
+            this.uiTabControl1.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTabControl1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(0, 40);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(548, 375);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "成员";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(0, 40);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(548, 375);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "角色";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // OrganizationUnitForm
             // 
+            this.AllowShowTitle = true;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.uiPanel2);
+            this.Controls.Add(this.uiPanelLeft);
             this.Controls.Add(this.uiPanel1);
             this.Name = "OrganizationUnitForm";
+            this.Padding = new System.Windows.Forms.Padding(0, 35, 0, 0);
+            this.ShowTitle = true;
             this.Text = "OrganizationUnitForm";
-            this.uiPanel2.ResumeLayout(false);
-            this.uiPanel3.ResumeLayout(false);
-            this.uiPanel3.PerformLayout();
+            this.uiPanelLeft.ResumeLayout(false);
+            this.uiPanelLeftHead.ResumeLayout(false);
+            this.uiPanelLeftHead.PerformLayout();
+            this.uiPanel1.ResumeLayout(false);
+            this.uiTabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
+        private Sunny.UI.UIPanel uiPanelLeft;
+        private Sunny.UI.UIPanel uiPanelLeftHead;
         private Sunny.UI.UIPanel uiPanel1;
-        private Sunny.UI.UIPanel uiPanel2;
-        private Sunny.UI.UIPanel uiPanel3;
-        private Sunny.UI.UILabel uiLabel1;
-        private Sunny.UI.UIButton uiButton1;
-        private Sunny.UI.UIButton uiButton2;
         private Sunny.UI.UITreeView uiTreeView1;
+        private Sunny.UI.UIButton uiButton2;
+        private Sunny.UI.UIButton uiButton1;
+        private Sunny.UI.UILabel uiLabel1;
+        private Sunny.UI.UITabControl uiTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
