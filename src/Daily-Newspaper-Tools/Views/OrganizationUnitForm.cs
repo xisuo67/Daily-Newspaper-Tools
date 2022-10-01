1f24330d92a6681835b8088d75b14ee39717be69
初十 <392706283@qq.com>
2022-10-01 09:59:34 +0800

    代码提交

--- src/Daily-Newspaper-Tools/Views/OrganizationUnitForm.cs
+++ src/Daily-Newspaper-Tools/Views/OrganizationUnitForm.cs
@@ -1,4 +1,5 @@
-﻿using Sunny.UI;
+﻿using DAL.Entity;
+using Sunny.UI;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -48,7 +49,53 @@ namespace Daily_Newspaper_Tools.Views
         #endregion
 
         #region 私有方法
+        /// <summary>
+        /// 新增节点
+        /// </summary>
+        /// <param name="department"></param>
+        private void Create(Department department)
+        {
+            department.Code = GetNextChildCode(department.ParentId);
+        }
 
+        /// <summary>
+        /// 校验
+        /// </summary>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        private bool ValidateOrganizationUnit(Department department)
+        {
+            return false;
+        }
+        /// <summary>
+        /// 查询子节点
+        /// </summary>
+        /// <param name="parentId"></param>
+        /// <param name="recursive"></param>
+        /// <returns></returns>
+        public List<Department> FindChildren(Guid? parentId, bool recursive = false)
+        {
+            return null;
+        }
+        /// <summary>
+        /// 获取code
+        /// </summary>
+        /// <param name="parentId"></param>
+        /// <returns></returns>
+        private string GetNextChildCode(Guid? parentId)
+        {
+            return null;
+        }
+        /// <summary>
+        /// 获取子节点code或根节点
+        /// </summary>
+        /// <param name="parentId"></param>
+        /// <returns></returns>
+
+        private Department GetLastChildOrNullAsync(Guid? parentId)
+        {
+            return null;
+        }
         #endregion
 
     }
