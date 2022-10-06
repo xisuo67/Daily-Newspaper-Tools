573ccf3bdc3e8bea4c10c782f77fe2171b0d6c5e
初十 <392706283@qq.com>
2022-10-06 15:12:47 +0800

    通用接口调整

--- src/Utility/Dependency/ILogin.cs
+++ src/Utility/Dependency/ILogin.cs
@@ -11,6 +11,30 @@ namespace Utility.Dependency
     /// </summary>
     public interface ILogin : ICommonLoginService
     {
-
+        /// <summary>
+        /// 获取第三方dom节点名字
+        /// </summary>
+        /// <returns></returns>
+        string GetAttribute();
+        /// <summary>
+        /// 获取Dom节点名字，用于判断qrcode是否取到
+        /// </summary>
+        /// <returns></returns>
+        string GetQrcodeDomAttribute();
+        /// <summary>
+        /// 获取code
+        /// </summary>
+        string GetCodeByUrl { get; set; }
+        /// <summary>
+        /// 获取Token
+        /// </summary>
+        /// <returns></returns>
+        string GetToken();
+        /// <summary>
+        /// 通过token获取用户ID
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        string FromTokenByUserId(string token);
     }
 }
