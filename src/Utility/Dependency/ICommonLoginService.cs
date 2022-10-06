1fe48ecc6fe6831aebe6be37ebf7bc18aabf727e
初十 <392706283@qq.com>
2022-10-06 14:39:30 +0800

    代码调整

---
+++ src/Utility/Dependency/ICommonLoginService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Dependency
+{
+    /// <summary>
+    ///  系统登录服务接口，主要用于规范接口，以便于提供服务工厂管理
+    ///      /// <code>
+    /// 实现本接口的类型规范：
+    /// 0.定义：实现类需要定义一个TypeKey，用于唯一识别当前服务
+    /// 1.调用：使用工厂创建实例，然后用接口接收调用
+    ///     1.1 访问示例：
+    ///         var instance = (IWorkWeChatLogin)LoginServiceFactory.Instance.GetInstances(LoginEnum.GlobalKey(WorkWeChatLogin));
+    /// </code>
+    /// </summary>
+    public interface ICommonLoginService
+    {
+    }
+}
