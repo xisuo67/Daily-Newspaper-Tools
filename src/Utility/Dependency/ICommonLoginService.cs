a14f8995fe96dd5e78038a451043c577d686ac40
初十 <392706283@qq.com>
2022-10-06 17:09:17 +0800

    代码调整

--- src/Utility/Dependency/ICommonLoginService.cs
+++ src/Utility/Dependency/ICommonLoginService.cs
@@ -14,10 +14,35 @@ namespace Utility.Dependency
     /// 0.定义：实现类需要定义一个TypeKey，用于唯一识别当前服务
     /// 1.调用：使用工厂创建实例，然后用接口接收调用
     ///     1.1 访问示例：
-    ///         var instance = (IWorkWeChatLogin)LoginServiceFactory.Instance.GetInstances(LoginEnum.GlobalKey(WorkWeChatLogin));
+    ///         var instance = (ICommonLoginService)LoginServiceFactory.Instance.GetInstances(LoginEnum.GlobalKey(WorkWeChatLogin));
     /// </code>
     /// </summary>
     public interface ICommonLoginService:IService
     {
+        /// <summary>
+        /// 获取第三方dom节点名字
+        /// </summary>
+        /// <returns></returns>
+        string GetAttribute();
+        /// <summary>
+        /// 获取Dom节点名字，用于判断qrcode是否取到
+        /// </summary>
+        /// <returns></returns>
+        string GetQrcodeDomAttribute();
+        /// <summary>
+        /// 获取code
+        /// </summary>
+        string GetCodeByUrl { get; set; }
+        /// <summary>
+        /// 获取Token
+        /// </summary>
+        /// <returns></returns>
+        string GetToken();
+        /// <summary>
+        /// 通过token获取用户ID
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        string FromTokenByUserId(string token);
     }
 }
