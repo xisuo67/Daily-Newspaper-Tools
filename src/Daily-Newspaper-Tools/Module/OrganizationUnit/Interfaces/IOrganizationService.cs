5a000e37d3239f820c0f0a9396f801f2857530ea
初十 <392706283@qq.com>
2022-10-07 20:26:27 +0800

    调整代码结构

---
+++ src/Daily-Newspaper-Tools/Module/OrganizationUnit/Interfaces/IOrganizationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Dependency;
+
+namespace Daily_Newspaper_Tools.Module.OrganizationUnit.Interfaces
+{
+    /// <summary>
+    /// 组织架构接口
+    /// </summary>
+    public interface IOrganizationService : IOrganizationUnitSyncService
+    {
+
+    }
+}
