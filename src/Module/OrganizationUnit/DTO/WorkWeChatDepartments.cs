533f83c83bc595868b13462bd3d86274ee4c8e99
初十 <392706283@qq.com>
2022-10-08 16:10:59 +0800

    增加注释

---
+++ src/Module/OrganizationUnit/DTO/WorkWeChatDepartments.cs
@@ -0,0 +1,39 @@
+﻿using Module.Login.DTO.WorkWeChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.OrganizationUnit.DTO
+{
+    /// <summary>
+    /// 企业微信部门
+    /// 接口文档：https://developer.work.weixin.qq.com/document/path/90208
+    /// </summary>
+    public class WorkWeChatDepartments
+    {
+        /// <summary>
+        /// 创建的部门id
+        /// </summary>
+        public string id { get; set; }
+        /// <summary>
+        /// 部门名称，代开发自建应用需要管理员授权才返回；此字段从2019年12月30日起，对新创建第三方应用不再返回，2020年6月30日起，对所有历史第三方应用不再返回name，返回的name字段使用id代替，后续第三方仅通讯录应用可获取，未返回名称的情况需要通过通讯录展示组件来展示部门名称
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 父部门id。根部门为1
+        /// </summary>
+        public string parentid { get; set; }
+
+        /// <summary>
+        /// 在父部门中的次序值。order值大的排序靠前。值范围是[0, 2^32)
+        /// </summary>
+        public string order { get; set; }
+    }
+
+    public class WorkWeChatDepartmentsDTO : ApiBaseResult
+    { 
+        public List<WorkWeChatDepartments> department { get; set; }
+    }
+}
--- src/Module/OrganizationUnit/DomainServices/WorkWeChatSyncOrganizationDomainService.cs
+++ src/Module/OrganizationUnit/DomainServices/WorkWeChatSyncOrganizationDomainService.cs
@@ -18,11 +18,23 @@ namespace Module.OrganizationUnit.DomainServices
         public static readonly string TypeKey = LoginEnum.GlobalKey(OrganizationUnitEnum.WorkWeChatSync);
         public List<Department> GetOrganizationSync()
         {
+            //企业微信同步组织架构接口文档地址：https://developer.work.weixin.qq.com/document/path/90208
+
+            //接口地址：https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token=ACCESS_TOKEN&id=ID
+
+           // 参数           必须    说明
+           //access_token    是    调用接口凭证
+           // id              否   部门id。获取指定部门及其下的子部门（以及子部门的子部门等等，递归）。 如果不填，默认获取全量组织架构
+
+
             throw new NotImplementedException();
         }
 
         public List<User> GetOrganizationUsersSync()
         {
+            //同步企业微信人员信息，接口文档地址：
+
+            //接口地址：https://qyapi.weixin.qq.com/cgi-bin/user/simplelist?access_token=ACCESS_TOKEN&department_id=DEPARTMENT_ID&fetch_child=1/0(1递归获取，0只获取本部门)
             throw new NotImplementedException();
         }
     }
