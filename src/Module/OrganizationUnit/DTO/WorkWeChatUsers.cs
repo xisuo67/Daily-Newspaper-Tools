a065a966196bbbfdabdc053771f5ec9ca457f242
初十 <392706283@qq.com>
2022-10-09 09:11:51 +0800

    增加实体

---
+++ src/Module/OrganizationUnit/DTO/WorkWeChatUsers.cs
@@ -0,0 +1,24 @@
+﻿using Module.Login.DTO.WorkWeChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.OrganizationUnit.DTO
+{
+    public class WorkWeChatUsers: ApiBaseResult
+    {
+        public List<WorkWeChatUsersDTO> userlist { get; set; }
+    }
+    public class WorkWeChatUsersDTO
+    { 
+        public string userid { get; set; }
+
+        public string name { get; set; }
+
+        public string[] department { get; set; }
+
+        public string open_userid { get; set; }
+    }
+}
