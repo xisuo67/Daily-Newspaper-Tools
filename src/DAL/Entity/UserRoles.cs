1a147e01cca120ecca1480df613b30599a5c21b0
初十 <392706283@qq.com>
2022-10-11 11:49:55 +0800

    增加实体

---
+++ src/DAL/Entity/UserRoles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entity
+{
+    public class UserRoles
+    {
+        /// <summary>
+        /// 主键id
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// 用户id
+        /// </summary>
+        public Guid UserId { get; set; }
+        /// <summary>
+        /// 角色id
+        /// </summary>
+
+        public Guid RoleId { get; set; }
+    }
+}
