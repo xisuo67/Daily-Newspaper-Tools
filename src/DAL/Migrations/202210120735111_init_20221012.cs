477826e1268db5cc8bc4227087d1a67f220416bd
初十 <392706283@qq.com>
2022-10-12 15:36:23 +0800

    代码调整

---
+++ src/DAL/Migrations/202210120735111_init_20221012.cs
@@ -0,0 +1,37 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init_20221012 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UserRoles",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        UserId = c.Guid(nullable: false),
+                        RoleId = c.Guid(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UserRoles");
+            DropTable("dbo.Roles");
+        }
+    }
+}
