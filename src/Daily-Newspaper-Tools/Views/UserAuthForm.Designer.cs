d5126df6d489947d440c9d44f60ac41cb265ee56
初十 <392706283@qq.com>
2022-10-19 21:58:37 +0800

    代码调整

--- src/Daily-Newspaper-Tools/Views/UserAuthForm.Designer.cs
+++ src/Daily-Newspaper-Tools/Views/UserAuthForm.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.uiTreeView1 = new Sunny.UI.UITreeView();
+            this.pnlBtm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBtm
+            // 
+            this.pnlBtm.Location = new System.Drawing.Point(1, 572);
+            this.pnlBtm.Size = new System.Drawing.Size(478, 55);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(350, 12);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(235, 12);
+            // 
+            // uiTreeView1
+            // 
+            this.uiTreeView1.FillColor = System.Drawing.Color.White;
+            this.uiTreeView1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTreeView1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiTreeView1.Location = new System.Drawing.Point(5, 5);
+            this.uiTreeView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTreeView1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTreeView1.Name = "uiTreeView1";
+            this.uiTreeView1.ShowText = false;
+            this.uiTreeView1.Size = new System.Drawing.Size(470, 557);
+            this.uiTreeView1.TabIndex = 2;
+            this.uiTreeView1.Text = "uiTreeView1";
+            this.uiTreeView1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uiTreeView1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // UserAuthForm
+            // 
+            this.AllowShowTitle = false;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(480, 630);
+            this.Controls.Add(this.uiTreeView1);
+            this.Name = "UserAuthForm";
+            this.Padding = new System.Windows.Forms.Padding(1, 0, 1, 3);
+            this.ShowTitle = false;
             this.Text = "UserAuthForm";
+            this.ZoomScaleRect = new System.Drawing.Rectangle(15, 15, 800, 450);
+            this.Controls.SetChildIndex(this.pnlBtm, 0);
+            this.Controls.SetChildIndex(this.uiTreeView1, 0);
+            this.pnlBtm.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Sunny.UI.UITreeView uiTreeView1;
     }
 }
