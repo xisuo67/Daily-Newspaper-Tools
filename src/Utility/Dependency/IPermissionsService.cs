3bad8ffbd40932ab68516e0a3280784db780220c
初十 <392706283@qq.com>
2022-10-25 17:04:42 +0800

    代码调整

---
+++ src/Utility/Dependency/IPermissionsService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Dependency
+{
+    public interface IPermissionsService: IService
+    {
+        /// <summary>
+        /// 初始化当前模块功能权限
+        /// </summary>
+        void InitCurrentModulePermission();
+
+
+    }
+}
