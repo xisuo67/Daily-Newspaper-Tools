7f4b54309add52c6616c85da2882d3a0b36a7460
初十 <392706283@qq.com>
2022-10-27 22:03:52 +0800

    代码提交

--- src/WindowsServiceClient/Form1.cs
+++ src/WindowsServiceClient/Form1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration.Install;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,11 +18,25 @@ namespace WindowsServiceClient
         public Form1()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
-
+        string serviceFilePath = $"{Application.StartupPath}\\AutoSendWeekly.exe";
+        string serviceName = "AutoSendWeeklyService";
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (this.IsServiceExisted(serviceName))
+                    this.UninstallService(serviceName);
+                else
+                    this.InstallService(serviceFilePath);
 
+                MessageBox.Show($"【{serviceName}】服务安装成功");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"安装服务失败：{ex.ToString()}");
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -36,5 +53,83 @@ namespace WindowsServiceClient
         {
 
         }
+        #region 服务私有方法
+        /// <summary>
+        /// 判断服务是否存在
+        /// </summary>
+        /// <param name="serviceName"></param>
+        /// <returns></returns>
+        private bool IsServiceExisted(string serviceName)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            foreach (ServiceController sc in services)
+            {
+                if (sc.ServiceName.ToLower() == serviceName.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 安装服务
+        /// </summary>
+        /// <param name="serviceFilePath"></param>
+        private void InstallService(string serviceFilePath)
+        {
+            using (AssemblyInstaller installer = new AssemblyInstaller())
+            {
+                installer.UseNewContext = true;
+                installer.Path = serviceFilePath;
+                IDictionary savedState = new Hashtable();
+                installer.Install(savedState);
+                installer.Commit(savedState);
+            }
+        }
+
+        /// <summary>
+        /// 卸载服务
+        /// </summary>
+        /// <param name="serviceFilePath"></param>
+        private void UninstallService(string serviceFilePath)
+        {
+            using (AssemblyInstaller installer = new AssemblyInstaller())
+            {
+                installer.UseNewContext = true;
+                installer.Path = serviceFilePath;
+                installer.Uninstall(null);
+            }
+        }
+        /// <summary>
+        /// 启动服务
+        /// </summary>
+        /// <param name="serviceName"></param>
+        private void ServiceStart(string serviceName)
+        {
+            using (ServiceController control = new ServiceController(serviceName))
+            {
+                if (control.Status == ServiceControllerStatus.Stopped)
+                {
+                    control.Start();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 停止服务
+        /// </summary>
+        /// <param name="serviceName"></param>
+        private void ServiceStop(string serviceName)
+        {
+            using (ServiceController control = new ServiceController(serviceName))
+            {
+                if (control.Status == ServiceControllerStatus.Running)
+                {
+                    control.Stop();
+                }
+            }
+        }
+        #endregion
     }
 }
