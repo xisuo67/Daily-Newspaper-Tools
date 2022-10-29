2ec8be56d4e05aeb3825ebe834bc08a44e0d7a4f
初十 <392706283@qq.com>
2022-10-29 12:58:37 +0800

    windows服务

--- src/AutoServices/Properties/AssemblyInfo.cs
+++ src/AutoServices/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("AutoService")]
+[assembly: AssemblyTitle("AutoServices")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("P R C")]
-[assembly: AssemblyProduct("AutoService")]
+[assembly: AssemblyProduct("AutoServices")]
 [assembly: AssemblyCopyright("Copyright © P R C 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("c4617f32-26f5-4263-bd8f-ce439de47f40")]
+[assembly: Guid("bd553886-fd01-427c-b83d-df162629506a")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
